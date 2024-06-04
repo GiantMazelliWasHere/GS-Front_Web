9339b7b1b91b2b57d0d4416986934cba45e5040e
eduardo <eduardo.mazelli@gmail.com>
2024-06-04 15:34:25 -0300

    Validação de login e recuperação de senha

--- ecosystem/src/Pages/EsqueceuSenha.jsx
+++ ecosystem/src/Pages/EsqueceuSenha.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {
   MDBBtn,
   MDBContainer,
   MDBCard,
   MDBCardBody,
   MDBInput,
-  MDBCheckbox
 }
 from 'mdb-react-ui-kit';
 
 function EsqueceuSenha() {
+    const [email, setEmail] = useState('');
+    const [nome, setNome] = useState('');
+    
+    const submitHandler = (e) => {
+        const alerta = document.createElement('h2');
+        const msg = document.getElementById('msg');
+        const alertaClass = alerta.className = ('alerta');
+        const limpeza = msg.innerHTML = '';
+        const limpeza2 = msg.style.backgroundColor = 'transparent';
+        
+        e.preventDefault();
+        if(nome === '') {
+            limpeza2;
+            alerta.innerHTML = 'Preencha seu Nome';
+            msg.style.backgroundColor = 'red';
+            return msg.appendChild(alerta);
+        }
+        if(email === '') {
+            limpeza2;
+            alerta.innerHTML = 'Preencha seu Email';
+            msg.style.backgroundColor = 'red';
+            return msg.appendChild(alerta);
+        }
+        alerta.innerHTML = 'Cheque seu Email';
+        msg.style.backgroundColor = 'green';
+        msg.appendChild(alerta);
+    }
+
     return ( 
         <MDBContainer fluid className='esqueceusenha d-flex align-items-center justify-content-center'>
             <div className='mask gradient-custom-3'></div>
-            <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+            <MDBCard className='m-5' style={{maxWidth: '350px'}}>
                 <MDBCardBody className='px-5'>
                     <h2 className="titulo text-uppercase text-center mb-5">Recuperação de Senha</h2>
-                    <MDBInput wrapperClass='mb-4' label='Nome' size='lg' id='form1' type='text'/>
-                    <MDBInput wrapperClass='mb-4' label='Email' size='lg' id='form2' type='email'/>
-                    <MDBBtn className='mb-4 gradient-custom-4 continue' size='lg'>Continue</MDBBtn>
+                    <MDBInput wrapperClass='mb-4' label='Nome' size='lg' id='form1' type='text' placeholder='Nome Completo' value={nome} onChange={(e) => setNome(e.target.value)}/>
+                    <MDBInput wrapperClass='mb-4' label='Email' size='lg' id='form2' type='email' placeholder='nome@hotmail.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <div className='alerta-esqueceu' id='msg'></div>
+                    <MDBBtn className='btn btn-primary continue' size='lg' type='submit' onClick={submitHandler}>Continue</MDBBtn>
                 </MDBCardBody>
             </MDBCard>
         </MDBContainer>
