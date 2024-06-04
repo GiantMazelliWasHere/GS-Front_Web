46b875fd2f9f203c94519d70311fd8fd72178a95
Nathan Uflacker <nathanuflacker@hotmail.com>
2024-06-04 15:45:11 -0300

    Responsividade Header

--- ecosystem/src/Components/Header/Header.jsx
+++ ecosystem/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header(){
         <>
         <header>
             <div>
-                <img src="./src/imgs/Titulo.png" alt="" />
+                <img src="./src/imgs/Titulo.png" alt="" className='img-ecosystem-header'/>
             </div>
             <div className="navegar">
                 <div className="home"><Link to={'/'}><a href="">Home</a></Link></div>
